5efd48ca1c440e98fb270ed54a4e29434b4a4401
scriptrdotio <git@apstrata.com>
2016-09-16 14:00:13 +0300

    updated

--- geoservices/simplemap/config.js
+++ geoservices/simplemap/config.js
@@ -16,7 +16,7 @@ var config = {
     lat: 40.674116, // sample, replace with your own
     lng: -74.005976, // sample, replace with your own
     zoom: 15, // sample, replace with your own
-    key: "AIzaSyBT2lgr9B0CzZSipJK9XSGllkfTUXICbQw", // sample, replace with your own key
+    key: "AIqaZyBX2lgd9c0CzZSipJK8XSGmlkfTUXICbQw", // sample, replace with your own key
     output: "terrain", // sample
     channel: "map", // The scriptr.io channel to subscribe the map to (it will receive data from that channel)
     callback: "" // you can specify your own callback function here (callback to be defined in datahandler.js)
