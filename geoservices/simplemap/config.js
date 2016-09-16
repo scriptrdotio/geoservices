49a49f6f8bf2268bb2ed40cf5f4d09cf45c8a4a1
scriptrdotio <git@apstrata.com>
2016-09-16 12:27:30 +0300

    updated

--- geoservices/simplemap/config.js
+++ geoservices/simplemap/config.js
@@ -1,20 +1,24 @@
+/**
+ * Configuration of the simplemap component
+ */ 
+
 var config = {
   
   // Your scriptr.io endpoint
   url: "wss://api.scriptrapps.io/", // do not forget the final "/" 
   
   // Token of your scriptr.io account
-  token: "VDNFQjUzMEQ3NA==",
+  token: "YOUR_SCRIPTR.IO_TOKEN",
   
   // Google map configuration
   map: {
     
-    lat: 40.674116,
-    lng: -74.005976,
-    zoom: 15,
-    key: "AIzaSyBT2lgr9B0CzZSipJK9XSGllkfTUXICbQw",
-    output: "terrain",
+    lat: 40.674116, // sample, replace with your own
+    lng: -74.005976, // sample, replace with your own
+    zoom: 15, // sample, replace with your own
+    key: "AIzaqyBx2ler9l0CxZSjpJK6XSGllkfTUXICbQw", // sample, replace with your own key
+    output: "terrain", // sample
     channel: "map", // The scriptr.io channel to subscribe the map to (it will receive data from that channel)
-    callback: ""
+    callback: "" // you can specify your own callback function here (callback to be defined in datahandler.js)
   }  
 };
